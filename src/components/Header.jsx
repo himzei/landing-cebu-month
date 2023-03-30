b8d087c89dcc0a5ef0fb01748f97d9b05273c534
조현일 <himzei@gmail.com>
2023-03-30 10:44:38 +0900

    scroll

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -46,7 +46,6 @@ export default function Header() {
 
   useEffect(() => {
     document.addEventListener("wheel", () => {
-      console.log(window.scrollY);
       if (window.scrollY < 10) {
         setScroll(true);
       } else {
@@ -67,16 +66,16 @@ export default function Header() {
         position="fixed"
         transition="0.4s"
         zIndex={999}
-        bg={scroll ? "transparent" : "gray.900"}
+        bg={scroll ? "transparent" || "gray.900" : "gray.900"}
       >
         <HStack justifyContent={"space-between"} w="7xl" h="full" px="2">
-          <Box h="full" p="4">
+          <Box h="full" px="4" py="5">
             <Link to="/">
               <Image h="70%" src={Logo} />
             </Link>
           </Box>
           <HStack spacing={8} transition="0.4s">
-            <HStack spacing={5} display={{ base: "none", lg: "block" }}>
+            <HStack spacing={5} display={{ sm: "none", lg: "block" }}>
               {menuList.map((_, i) =>
                 pathname === "/" ? (
                   <LinkScroll
@@ -114,8 +113,10 @@ export default function Header() {
             </HStack>
             <HStack spacing={5}>
               <a href="/#6">
-                <Button bg="yellow.600">
-                  <Text>빠른견적 문의</Text>
+                <Button bg="yellow.600" w={{ sm: "100px", md: "120px" }}>
+                  <Text fontSize={{ sm: "14px", md: "16px" }}>
+                    빠른견적 문의
+                  </Text>
                 </Button>
               </a>
               <Box
